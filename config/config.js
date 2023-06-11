112b581fbc51c11002c7b56455b1818e01d25170
billy <billynatanael91@gmail.com>
2023-06-11 21:22:37 +0700

    database URL

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const dbConfig = {
-  HOST: 'mysql://root:vZQ3riCjrZo7QD1gIa2R@containers-us-west-97.railway.app:5994/railway',
+  HOST: 'containers-us-west-97.railway.app',
   USER: 'root',
   PASSWORD: 'vZQ3riCjrZo7QD1gIa2R',
   DB: 'railway',
